a7e0bdf45b33e98955227299a7279bbdfd1ac705
김연수 <yeonsoo9846@gmail.com>
2022-05-07 22:20:02 +0900

    예제 풀이

--- index.ts
+++ index.ts
@@ -83,6 +83,30 @@ type Person = Name | Age;
 
 type PositionX = {x : number};
 type PositionY = {y : number};
+type PositionX2 = {x : number}
 type NewType = PositionX & PositionY;
+type NewType2 = PositionX & PositionX2;
 
 let position : NewType = {x: 10, y:20}
+let position2 : NewType2 = {x: 20}
+
+//이 타입은 object 자료형이어야합니다.
+//이 타입은 color 라는 속성을 가질 수도 있으며 항상 문자가 들어와야합니다. 
+//이 타입은 size 라는 속성이 있어야하며 항상 숫자가 들어와야합니다.
+//이 타입은 position 이라는 변경불가능한 속성이 있어야하며 항상 숫자가 담긴 array 자료가 들어와야합니다.  
+type ThatType = {
+    color? : string , 
+    size : number, 
+    readonly position :number[]
+};
+
+//대충 이렇게 생긴 object 자료를 다룰 일이 많습니다. { name : 'kim', phone : 123, email : 'abc@naver.com' }
+//object 안에 있는 이름, 전화번호, 이메일 속성이 옳은 타입인지 검사하는 type alias를 만들어봅시다.
+//각 속성이 어떤 타입일지는 자유롭게 정하십시오. 
+type ConfirmData = {name : string, phone : number, email : string}
+
+//이름, 전화번호, 이메일, 미성년자여부 속성을 옳은 타입인지 검사하는 type alias를 만들어봅시다.
+//미성년자 여부 속성은 true/false만 들어올 수 있습니다. 
+// type alias를 재활용
+type addAdult = {adult : boolean}
+type ConfirmData2 = ConfirmData & addAdult;
